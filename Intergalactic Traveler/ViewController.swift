e8e854668a6a56e9a578232aeb3812c768b20ad4
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-03-08 14:14:22 -0700

    two buttons

--- Intergalactic Traveler/ViewController.swift
+++ Intergalactic Traveler/ViewController.swift
@@ -12,8 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        assignbackground()
     }
-
+func assignbackground()
+    {
+        let background = UIImage(named: "Title")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode = UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
 
 }
 
